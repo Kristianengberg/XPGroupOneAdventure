6743d3ca9d747768f54e67578caa45aebcd8a320
Kristian Engberg <krissoengberg@gmail.com>
2020-09-23 13:14:52 +0200

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+}
