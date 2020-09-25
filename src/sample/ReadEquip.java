d26564a43f8e8588d95a9a2ab30b2b5333f2ced0
JakobBundgaard <56150649+JakobBundgaard@users.noreply.github.com>
2020-09-25 09:46:12 +0200

    New test

---
+++ src/sample/ReadEquip.java
@@ -0,0 +1,18 @@
+package sample;
+
+import models.EquipmentModel;
+
+import java.util.ArrayList;
+
+public class ReadEquip {
+
+    private String id;
+    private String type;
+    private boolean needsRepair;
+
+    public Object readEquipment(ArrayList<EquipmentModel> em) {
+
+        throw new java.lang.UnsupportedOperationException("Method not yey implemented");
+    }
+
+}
--- src/sample/ReadEquipTest.java
+++ src/sample/ReadEquipTest.java
@@ -8,7 +8,9 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class readEquipTest {
+class ReadEquipTest {
+
+    private Object EquipmentModel;
 
     @Test
     public void testAll() {
@@ -35,9 +37,32 @@ class readEquipTest {
         equipment.add(new EquipmentModel("gk4", "Gokart", false ));
         return equipment.get(0);
     }
+    // Sidste
+    @Test
+    public void testRead() {
+        ArrayList<models.EquipmentModel> equipment1 = new ArrayList<>();
+        equipment1.add(new EquipmentModel("gk1", "Gokart", false ));
+        equipment1.add(new EquipmentModel("gk2", "Gokart", false ));
+        equipment1.add(new EquipmentModel("gk3", "Gokart", false ));
+        equipment1.add(new EquipmentModel("gk4", "Gokart", false ));
+
+        ReadEquip readEquip = new ReadEquip();
+        Object actual = equipment1;
+        Object expected = readEquip.readEquipment(equipment1);
+
+        assertEquals(actual, expected);
+
+    }
+
 
     @Test
     public List readEquip(List<EquipmentModel> em)  {
+            em.add(new EquipmentModel("gk1", "Gokart", false ));
+            List actual = em;
+
+        ReadEquipTest readEquipTest = new ReadEquipTest();
+            List result = readEquipTest.readEquip(em);
+            assertEquals(actual, result);
 
 
         return em;
